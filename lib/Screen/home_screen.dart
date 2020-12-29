b145c9c8ce1feafe03d3e77c04815f7107e95d42
Jpilosel <joel@pilosel.fr>
2020-12-29 10:25:54 +0100

    import image et mise en place de l'animation de rotation

--- lib/Screen/home_screen.dart
+++ lib/Screen/home_screen.dart
@@ -14,16 +14,29 @@ class _HomeScreenState extends State<HomeScreen>
   @override
   void initState() {
     _animationController = AnimationController(
-        vsync: this, duration: Duration(milliseconds: 2000));
+        vsync: this, duration: Duration(milliseconds: 5000));
     _animation = CurvedAnimation(
         parent: _animationController, curve: Curves.bounceInOut);
+    _animationController.forward();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(child: Text("Home")),
+      body: Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.symmetric(horizontal: 10.0),
+        child: Stack(
+          children: <Widget>[
+            Image.asset('assets/wheel.png'),
+            RotationTransition(
+              turns: _animation,
+              child: Image.asset('assets/arrow.png'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
