56309fd402038b2a9e8a76ca6c2aa7d91a86035f
Jpilosel <joel@pilosel.fr>
2020-12-29 09:50:27 +0100

    init Animation

--- lib/Screen/home_screen.dart
+++ lib/Screen/home_screen.dart
@@ -5,7 +5,21 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  // Si plusieurs annimation dans une page utiliser TickerProviderStateMixin
+  AnimationController _animationController;
+  Animation _animation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 2000));
+    _animation = CurvedAnimation(
+        parent: _animationController, curve: Curves.bounceInOut);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
